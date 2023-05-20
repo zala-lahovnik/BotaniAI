d8d1d94214eb39335ae6c3b439a43bada8dff605
JevsnikJaka <74906843+JevsnikJaka@users.noreply.github.com>
2023-05-20 11:28:16 +0200

    feat(backend): add DELETE for personalGarden plant

--- Backend/routes/userPlant.js
+++ Backend/routes/userPlant.js
@@ -128,4 +128,37 @@ router.get('/history/:userId', (req, res) => {
     });
 });
 
+
+router.delete('/personalGarden/:userId/:plantId', (req, res) => {
+
+  const db = getDB();
+  const userId = req.params.userId;
+  const plantId = req.params.plantId;
+
+  const userCollection = db.collection('user');
+
+  userCollection.findOne({ _id: userId })
+  .then(user => {
+
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    const personalGarden = user.personalGarden;
+    const updatedGarden = personalGarden.filter(obj => !obj._id.toString() === (plantId));
+
+    userCollection.updateOne(
+      { _id: userId },
+      { $set: { personalGarden: updatedGarden } }
+    ).then(() => {
+      res.status(200).send('Plant deleted from personal garden');
+    });
+  })
+  .catch (err => {
+    console.error('Failed to delete plant from personal garden:', err);
+    res.status(500).send('Failed to delete plant from personal garden');
+  });
+});
+
+
 module.exports = router;
