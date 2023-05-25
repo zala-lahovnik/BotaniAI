1105d7a7b33ae564447ce494a7b6dfcc9a26a6e0
anja <anja.borak1@student.um.si>
2023-05-25 23:35:19 +0200

    feat(frontend): add google sign in

--- Frontend/screens/LoginScreen/LoginScreen.tsx
+++ Frontend/screens/LoginScreen/LoginScreen.tsx
@@ -1,23 +1,32 @@
-import { Text, View, TextInput, Pressable, } from 'react-native';
-import { useState } from "react";
+import { Text, View, TextInput, Pressable, Button } from 'react-native';
+import { useState, useEffect } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 import { Ionicons } from '@expo/vector-icons';
 import { styles } from './LoginScreenStyles';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { GOOGLE_EXPO, GOOGLE_ANDROID, GOOGLE_IOS } from '../../firebase/firebase-config';
+
+
+WebBrowser.maybeCompleteAuthSession();
+
+
+
+
 
 type Props = NativeStackScreenProps<any>;
 export const LoginScreen = ({ navigation }: Props) => {
-
+    const [token, setToken] = useState("");
+    const [userInfo, setUserInfo] = useState(null);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
     const toggleShowPassword = () => {
         setShowPassword(!showPassword);
     };
-    function handleLoginGoogle() {
-        //google login???
-    }
+
     function handleRegister() {
         navigation.navigate('RegisterScreen')
     }
@@ -35,9 +44,46 @@ export const LoginScreen = ({ navigation }: Props) => {
             .then((userCredentials: { user: any; }) => {
                 const user = userCredentials.user;
                 console.log("Logged in with : ", user.email);
+                setUserInfo(user)
             }).then(() => navigation.navigate('PlantListScreen'))
             .catch((error: { message: any; }) => alert(error.message));
     }
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        androidClientId: GOOGLE_ANDROID,
+        iosClientId: GOOGLE_IOS,
+        expoClientId: GOOGLE_EXPO
+    });
+
+    useEffect(() => {
+        if (response?.type === "success") {
+            setToken(response.authentication.accessToken);
+            getUserInfo();
+        }
+    }, [response, token]);
+
+
+    const getUserInfo = async () => {
+        try {
+            const response = await fetch(
+                "https://www.googleapis.com/userinfo/v2/me",
+                { headers: { Authorization: `Bearer ${token}` }, }
+            );
+            const user = await response.json().then();
+            setUserInfo(user);
+
+            navigation.navigate('Home');
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+
+    if (userInfo) {
+        navigation.navigate('Home');
+    }
+
+
     return (
         <View style={styles.container}>
             <Pressable style={styles.puscica} onPress={handleBack}>
@@ -78,11 +124,21 @@ export const LoginScreen = ({ navigation }: Props) => {
             <Pressable style={styles.button2} onPress={handleRegister}>
                 <Text style={styles.buttonText}>Create an account</Text>
             </Pressable>
-            <Pressable style={styles.button2} onPress={handleLoginGoogle}>
+            <Pressable style={styles.button2} onPress={
+                () => { promptAsync(); }}>
                 <View style={styles.googleButtonContainer}>
                     <Ionicons name="logo-google" size={21} color="white" style={styles.icon2} />
                     <Text style={styles.buttonText}>Continue with Google</Text>
                 </View>
+
+
+
+
+
+
+
+
+
             </Pressable>
             <Pressable onPress={handlePassword}>
                 <Text style={[styles.text, styles.lined]}> FORGOT YOUR PASSWORD?</Text>
